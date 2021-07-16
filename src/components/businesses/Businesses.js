5ca29c3b8bf759a9c6b17db3548a6a8211f31f73
Anthony <anthonydbeck77@gmail.com>
2021-07-16 14:56:11 +1000

    Added sub-components for Business views

--- src/components/businesses/Businesses.js
+++ src/components/businesses/Businesses.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+import BusinessCard from './BusinessCard';
+import BusinessSearchFilter from './BusinessSearchFilter';
 
 const Businesses = () => {
   const [businesses, setBusinesses] = useState([]);
@@ -15,23 +16,11 @@ const Businesses = () => {
   return (
     <div>
       <h1>Browse Businesses</h1>
-      <code>Search Form</code>
+      <BusinessSearchFilter />
       <br />
       { businesses.map(business => (
         <>
-          <h2>{business.name}</h2>
-          <h3>{business.category.name}</h3>
-          <p>{`${business.description.substr(0, 60)}...`}</p>
-          <img src="https://placekitten.com/200/200" />
-
-          <p>
-            {business.address.street}&nbsp;
-            {business.address.suburb.name}&nbsp;
-            {business.address.postcode.code}&nbsp;
-            {business.address.state.name}
-          </p>
-
-          <Link to={`/business/${business.id}`}>Know more</Link>
+          <BusinessCard business={business} />
         </>
       )
       )}
