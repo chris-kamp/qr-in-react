15140cc7384a23610697afcf71acf5ac40136c02
Anthony <anthonydbeck77@gmail.com>
2021-07-16 14:36:48 +1000

    Implemented business components - Added simple views for Businesses. - Changed package for Google Places Autocomplete, awaiting API configuration before moving ahead with this feature.

--- src/components/Businesses.js
+++ src/components/Businesses.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
 const Businesses = () => {
+  const [businesses, setBusinesses] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_ENDPOINT}/businesses`)
+      .then(response => {
+        setBusinesses(response.data);
+      });
+  }, []);
+
   return (
     <div>
-      <h1>Businesses</h1>
+      <h1>Browse Businesses</h1>
+      <code>Search Form</code>
+      <br />
+      { businesses.map(business => (
+        <>
+          <h2>{business.name}</h2>
+          <h3>{business.category.name}</h3>
+          <p>{`${business.description.substr(0, 60)}...`}</p>
+          <img src="https://placekitten.com/200/200" />
+
+          <p>
+            {business.address.street}&nbsp;
+            {business.address.suburb.name}&nbsp;
+            {business.address.postcode.code}&nbsp;
+            {business.address.state.name}
+          </p>
+
+          <Link to={`/business/${business.id}`}>Know more</Link>
+        </>
+      )
+      )}
     </div>
   )
 }
