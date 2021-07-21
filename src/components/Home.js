30d912d5991455022e261390b4df33e2b17558b4
Chris Kamp <chriskamp101@gmail.com>
2021-07-22 08:59:26 +1000

    add image upload widget as placeholder on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,28 @@
 import { stateContext } from "../stateReducer"
 import { useContext } from "react"
+import {Image} from 'cloudinary-react';
 
 const Home = () => {
   const { session } = useContext(stateContext)
+  let widget = window.cloudinary.createUploadWidget(
+    {
+      cloudName: "chriskamp",
+      uploadPreset: "gp17ernf",
+      folder: "qrin"
+    },
+    (error, result) => {
+      if (!error && result && result.event === "success") {
+        console.log("Done! Here is the image info: ", result.info)
+      }
+    }
+  )
+  const showWidget = () => widget.open()
 
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={showWidget}>Upload Image</button>
+      <Image cloudName="chriskamp" publicId="qrin/wfjxocf60izkkadk1hpz" width="300" crop="scale"/>
     </div>
   )
 }
