edc843a1c6893ff51595f8c8ff8df2c26893f1c6
Chris Kamp <chriskamp101@gmail.com>
2021-07-22 15:18:28 +1000

    implement placeholder image for users without profile picture

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,7 +7,10 @@ import { useParams } from "react-router"
 import axios from "axios"
 import PageHeading from "./shared/PageHeading"
 import { useHistory } from "react-router-dom"
-import { createProfileImgWidget, getProfileImgWidgetOpener } from "../utils/CloudinaryWidgets"
+import {
+  createProfileImgWidget,
+  getProfileImgWidgetOpener,
+} from "../utils/CloudinaryWidgets"
 
 const Profile = () => {
   const { session, dispatch } = useContext(stateContext)
@@ -44,37 +47,43 @@ const Profile = () => {
       <PageHeading>Profile</PageHeading>
       <Columns className="is-vcentered">
         <Columns.Column>
-          {user?.profile_img_src ? (
-            <Image
-              cloudName="chriskamp"
-              publicId={user.profile_img_src}
-              width="300"
-              crop="scale"
-              style={{ display: "block", borderRadius: "50%" }}
-              className="mx-auto"
-            />
-          ) : (
-            <p>Image placeholder</p>
+          <Image
+            cloudName="chriskamp"
+            publicId={
+              user?.profile_img_src
+                ? user.profile_img_src
+                : "v1626930815/qrin/profile-pic-placeholder_rwztji"
+            }
+            width="300"
+            crop="scale"
+            style={{ display: "block", borderRadius: "50%" }}
+            className="mx-auto"
+          />
+          {session?.user.id.toString() === id && (
+            <Button
+              className="button has-background-primary-dark has-text-white has-text-weight-bold mx-auto mt-2"
+              style={{ borderRadius: "0.6rem", display: "block" }}
+              onClick={showWidget}
+            >
+              Update Profile Picture
+            </Button>
           )}
-          {session?.user.id.toString() === id && (<Button
-            className="button has-background-primary-dark has-text-white has-text-weight-bold mx-auto mt-2"
-            style={{ borderRadius: "0.6rem", display: "block" }}
-            onClick={showWidget}
-          >
-            Update Profile Picture
-          </Button>)}
         </Columns.Column>
         <Columns.Column>
           <Heading className="is-size-4 has-text-centered-mobile">Bio</Heading>
           {user?.bio ? (
             <p className="has-text-centered-mobile">{user.bio}</p>
           ) : (
-            <p className="has-text-centered-mobile">This user hasn't created a bio yet!</p>
+            <p className="has-text-centered-mobile">
+              This user hasn't created a bio yet!
+            </p>
           )}
         </Columns.Column>
       </Columns>
       <section>
-        <Heading className="is-size-4 has-text-centered mt-4">Recent Checkins</Heading>
+        <Heading className="is-size-4 has-text-centered mt-4">
+          Recent Checkins
+        </Heading>
       </section>
     </Container>
   )
