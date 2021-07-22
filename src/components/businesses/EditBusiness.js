693d3b9f1c8ffdcd839404880e532f58041c855c
Anthony <anthonydbeck77@gmail.com>
2021-07-23 00:55:01 +1000

    Refactor edit business form

--- src/components/businesses/EditBusiness.js
+++ src/components/businesses/EditBusiness.js
@@ -3,19 +3,80 @@ import { useHistory, useParams } from "react-router"
 import axios from "axios"
 import { useForm } from "react-hook-form"
 import { stateContext } from "../../stateReducer"
-import { Container, Card, Heading, Button } from "react-bulma-components"
 import LocationAutocomplete from "./LocationAutocomplete"
+import { enforceLogin } from "../../utils/Utils"
+import FormContainer from "../shared/FormContainer"
+import PageHeading from "../shared/PageHeading"
+import InputLabel from "../shared/InputLabel"
+import Input from "../shared/Input"
+import { ErrorText } from "../../styled-components/FormStyledComponents"
+import TextArea from "../shared/TextArea"
+import Select from "../shared/Select"
+import FormButtonGroup from "../shared/FormButtonGroup"
+import Checkbox from "../shared/Checkbox"
 
 const EditBusiness = () => {
-  const context = useContext(stateContext)
-  const { dispatch } = useContext(stateContext)
-  const { register, handleSubmit, getValues, setValue, formState: { errors } } = useForm()
-  const [useManualAddress, setUseManualAddress] = useState(false)
   const [categories, setCategories] = useState([])
-  const { id } = useParams()
+  const [failureMessage, setFailureMessage] = useState()
   const history = useHistory()
+  const { id } = useParams()
+  const { session, dispatch } = useContext(stateContext)
+  const { register, watch, setValue, handleSubmit, formState: { errors } } = useForm()
+  const watchManualAddress = watch('manualAddress', false)
+
+  const onSubmit = (data) => {
+    // Ensure user is logged in
+    enforceLogin("You must be logged in to create a business.", session, dispatch, history)
+
+    // Get the user id from the current session
+    data.business.user_id = session.user.id
+
+    // Send the data to Rails API
+    axios.patch(`${process.env.REACT_APP_API_ENDPOINT}/businesses/${id}`, data, {
+      headers: { Authorization: `Bearer ${session?.token}` }
+    })
+      .then((response) => {
+        console.debug(response)
+
+        dispatch({
+          type: 'pushAlert',
+          alert: {
+            message: 'Business updated successfully',
+            type: 'notice'
+          }
+        })
+
+        setFailureMessage(null)
+
+        // View the business with the ID returned from Rails
+        history.push(`/businesses/${response.data.id}`)
+      })
+      .catch((err) => {
+        // Display the errors to the user
+        dispatch({
+          type: 'pushAlert',
+          alert: {
+            // message: 'Errors: ' + Object.keys(err.response.data.errors).map(k => `${k}: ${err.response.data.errors[k].join(', ')}`).join(', '),
+            message: JSON.stringify(err.response.data),
+            type: 'error'
+          }
+        })
+      })
+  }
 
   useEffect(() => {
+    enforceLogin("You must be logged in to create a business.", session, dispatch, history)
+
+    axios.get(`${process.env.REACT_APP_API_ENDPOINT}/categories`)
+      .then((response) => {
+        setCategories(response.data)
+      })
+      .catch((err) => {
+        setFailureMessage(
+          "Something went wrong. Please try again shortly."
+        )
+      })
+
     axios.get(`${process.env.REACT_APP_API_ENDPOINT}/businesses/${id}`)
       .then((response) => {
         setValue('business', {
@@ -27,120 +88,111 @@ const EditBusiness = () => {
           }
         })
       })
-  }, [])
-
-
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_API_ENDPOINT}/categories`)
-      .then(response => {
-        setCategories(response.data)
-      })
-  }, [])
-
-  const onSubmit = (data) => {
-    data.business.user_id = context.session.user.id
-
-    axios
-      .patch(`${process.env.REACT_APP_API_ENDPOINT}/businesses/${id}`, data)
-      .then((response) => {
-        dispatch({
-          type: 'pushAlert',
-          alert: {
-            message: 'Business updated successfully',
-            type: 'notice'
-          }
-        })
-
-        history.push(`/businesses/${response.data.id}`)
-      })
-      .catch((error) => {
-        dispatch({
-          type: 'pushAlert',
-          alert: {
-            message: 'Errors: ' + Object.keys(error.response.data.errors).map(k => `${k}: ${error.response.data.errors[k].join(', ')}`).join(', '),
-            type: 'error'
-          }
-        })
-      })
-  }
+  }, [dispatch, history, session])
 
   return (
-    <Container>
-      <Card>
-        <Card.Header.Title>Edit Business</Card.Header.Title>
-        <Card.Content>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Heading size={5}>Business details</Heading>
+    <FormContainer>
+      <PageHeading>Edit Business</PageHeading>
+      <form onSubmit={handleSubmit(onSubmit)} id="newBusinessForm" />
+      <InputLabel htmlFor="business.name" text="Name" isFirst />
+      <Input
+        register={register}
+        validator={{ required: true }}
+        name="business.name"
+        placeholder="Your Business"
+        form="newBusinessForm"
+        focus
+      />
+      {errors.business?.name && <ErrorText>Invalid business name</ErrorText>}
 
-            <div className="control">
-              <label className="label" htmlFor="business.name">Name</label>
-              <input className="input" type="text" id="name" {...register('business.name')} />
-              {errors.business?.name && 'Name is required'}
-            </div>
+      <InputLabel htmlFor="business.description" text="Description" />
+      <TextArea
+        register={register}
+        validator={{ required: true }}
+        name="business.description"
+        placeholder="A description of your business."
+        form="newBusinessForm"
+      />
+      {errors.business?.description && <ErrorText>Invalid business description</ErrorText>}
 
-            <div className="control">
-              <label className="label" htmlFor="business.description">Description</label>
-              <textarea className="input" type="text" id="description" {...register('business.description')}></textarea>
-              {errors.business?.description && 'Description is required'}
-            </div>
+      <InputLabel htmlFor="business.category_id" text="Category" />
+      <Select
+        register={register}
+        validator={{ required: true }}
+        name="business.category_id"
+        options={categories}
+        optionKeyField="id"
+        optionLabelField="name"
+        form="newBusinessForm"
+      />
+      {errors.business?.category && <ErrorText>Invalid category</ErrorText>}
 
-            <div className="control">
-              <label className="label" htmlFor="business.category_id">Category</label>
-              <select className="select is-fullwidth" {...register('business.category_id')}>
-                {categories.map(category => <option key={category.id} value={category.id}>{category.name}</option>)}
-              </select>
-            </div>
+      <InputLabel htmlFor="business.image" text="Upload business image" />
+      <code>image upload</code>
 
-            <br />
+      <InputLabel />
+      <Checkbox
+        register={register}
+        name="manualAddress"
+        labelText="Manual address"
+        type="checkbox"
+        form="newBusinessForm"
+      />
 
-            <Heading size={5}>Address</Heading>
+      {watchManualAddress ? (
+        <React.Fragment>
+          <InputLabel htmlFor="business.address.street" text="Street address" />
+          <Input
+            register={register}
+            validator={{ required: true }}
+            name="business.address.street"
+            placeholder="123 Coder St"
+            form="newBusinessForm"
+          />
+          {errors.business?.address?.street && <ErrorText>Invalid street</ErrorText>}
 
-            {!useManualAddress && (
-              <React.Fragment>
-                <LocationAutocomplete addressCallback={(address) => {
-                  setValue('business.address', address)
-                }} />
-              </React.Fragment>
-            )}
+          <InputLabel htmlFor="business.address.suburb" text="City or suburb" />
+          <Input
+            register={register}
+            validator={{ required: true }}
+            name="business.address.suburb"
+            placeholder="Brisbane City"
+            form="newBusinessForm"
+          />
+          {errors.business?.address?.suburb && <ErrorText>Invalid suburb</ErrorText>}
 
-            <div className="is-pulled-right control">
-              <label className="checkbox" htmlFor="useManualAddress">
-                <input className="checkbox mr-2" id="useManualAddress" onChange={() => { setUseManualAddress(!useManualAddress) }} type="checkbox" />
-                Enter address manually?
-              </label>
-            </div>
+          <InputLabel htmlFor="business.address.postcode" text="Postal code" />
+          <Input
+            register={register}
+            validator={{ required: true }}
+            name="business.address.postcode"
+            placeholder="4000"
+            form="newBusinessForm"
+          />
+          {errors.business?.address?.postcode && <ErrorText>Invalid postcode</ErrorText>}
 
-            {useManualAddress && (
-              <React.Fragment>
-                <div className="control">
-                  <label htmlFor="business.address.street" className="label">Street Address</label>
-                  <input className="input" type="text" id="street" {...register('business.address.street')} />
-                </div>
+          <InputLabel htmlFor="business.address.state" text="State" />
+          <Input
+            register={register}
+            validator={{ required: true }}
+            name="business.address.state"
+            placeholder="QLD"
+            form="newBusinessForm"
+          />
+          {errors.business?.address?.state && <ErrorText>Invalid state</ErrorText>}
+        </React.Fragment>
+      ) : (
+        <LocationAutocomplete addressCallback={(address) => {
+          setValue('business.address', address)
+        }} />
+      )}
 
-                <div className="control">
-                  <label htmlFor="business.address.suburb" className="label">Suburb / City</label>
-                  <input className="input" type="text" id="suburb" {...register('business.address.suburb')} />
-                </div>
+      {failureMessage && (
+        <ErrorText>Business creation failed: {failureMessage}</ErrorText>
+      )}
+      <FormButtonGroup form="newBusinessForm" submitValue="Save business" />
 
-                <div className="control">
-                  <label htmlFor="business.address.postcode" className="label">Postcode</label>
-                  <input className="input" type="text" id="postcode" {...register('business.address.postcode')} />
-                </div>
-
-                <div className="control">
-                  <label htmlFor="business.address.state" className="label">State</label>
-                  <input className="input" type="text" id="state" {...register('business.address.state')} />
-                </div>
-              </React.Fragment>
-            )}
-
-            <br />
-
-            <Button submit color='primary'>Update Business</Button>
-          </form>
-        </Card.Content>
-      </Card>
-    </Container>
+    </FormContainer>
   )
 }
 
