f435e97c98af5407671f0bb48601c1754670f47d
Anthony <anthonydbeck77@gmail.com>
2021-07-26 14:09:15 +1000

    Cypress testing for businesses Viewing and creating A business

--- cypress/integration/business-components/business.spec.js
+++ cypress/integration/business-components/business.spec.js
@@ -18,7 +18,7 @@ describe('Businesses', () => {
     })
 
     it('Searches businesses and displays the correct results', () => {
-        cy.fixture('business-search_name').then((businessesJson) => {
+        cy.fixture('business-search-name').then((businessesJson) => {
             cy.intercept(`${Cypress.env('apiUrl')}/businesses/search*`, {
                 statusCode: 200,
                 body: businessesJson
@@ -30,17 +30,22 @@ describe('Businesses', () => {
     })
 
     it('Uses checkboxes to filter the search', () => {
-        cy.fixture('business-search_filter').then((businessesJson) => {
+        cy.fixture('categories-index').then((categoriesJson) => {
+            cy.intercept(`${Cypress.env('apiUrl')}/categories`, {
+                statusCode: 200,
+                body: categoriesJson
+            })
+        })
+
+        cy.fixture('business-search-filter').then((businessesJson) => {
             cy.intercept(`${Cypress.env('apiUrl')}/businesses/search*`, {
                 statusCode: 200,
                 body: businessesJson
             })
+            cy.get('.is-pulled-right > :nth-child(4) > .checkbox').click()
+            cy.get(':nth-child(2) > .is-primary').click()
+            cy.get('.card > :nth-child(3)').should('have.text', "It's the first business...")
         })
-
-        cy.get('.is-pulled-right > :nth-child(4) > .checkbox').click()
-        cy.get(':nth-child(2) > .is-primary').click()
-
-        cy.get('.card > :nth-child(3)').should('have.text', "It's the first business...")
     })
 })
 
@@ -89,5 +94,88 @@ describe('Business', () => {
         cy.get(':nth-child(3) > :nth-child(2) > :nth-child(6)').contains('Extremely average')
 
         cy.get(':nth-child(3) > :nth-child(2) > .is-pulled-right').should('be.visible')
+    })
+})
+
+describe('Business Owner', () => {
+    it('Login as user', () => {
+        cy.visit(`${Cypress.env('siteUrl')}/login`)
+
+        cy.get('[placeholder="email@example.com"]').type('user4@user4.com')
+        cy.get('[placeholder="Password"]').type('Password1')
+
+        cy.fixture('user-login').then((userLoginJson) => {
+            cy.intercept('POST', `${Cypress.env('apiUrl')}/users/login`, {
+                statusCode: 200,
+                body: userLoginJson
+            })
+
+            cy.get('.has-background-primary-dark').click()
+        })
+    })
+
+    it('Creates a business from Profile page', () => {
+        cy.fixture('user-show').then((userJson) => {
+            cy.intercept(`${Cypress.env('apiUrl')}/users/4`, {
+                statusCode: 200,
+                body: userJson
+            })
+            cy.intercept(`${Cypress.env('apiUrl')}/users/4/public`, {
+                statusCode: 200,
+                body: userJson
+            })
+
+            cy.get('[href="/users/4"]').click()
+        })
+
+
+        cy.fixture('categories-index').then((categoriesJson) => {
+            cy.intercept(`${Cypress.env('apiUrl')}/categories`, {
+                statusCode: 200,
+                body: categoriesJson
+            })
+        })
+        // Navigate to new business page
+        cy.get('.container > .is-flex > .button').click()
+
+        // Name
+
+        // Description
+        cy.get('.textarea').type("It's the fourth business...")
+
+        // Category
+        cy.get('.select').select("1")
+
+        cy.get('.mr-1').click()
+
+        cy.get('[placeholder="Brisbane City"]').type('Brisbane City')
+        cy.get('[placeholder="QLD"]').type('QLD')
+
+        // Errors
+        cy.get('div.mt-5 > .has-background-primary-dark').click()
+
+        cy.get('.container > :nth-child(5)').should('have.text', 'Invalid business name')
+        cy.get('.container > :nth-child(14)').should('have.text', 'Invalid street')
+        cy.get('.container > :nth-child(19)').should('have.text', 'Invalid postcode')
+
+        cy.get('[placeholder="Your Business"]').type('Fourth business')
+        cy.get('[placeholder="123 Coder St"]').type('116 Adelaide St')
+        cy.get('[placeholder="4000"]').type('4000')
+
+        cy.fixture('business-create-post').then((businessCreatePostJson) => {
+            cy.intercept('POST', `${Cypress.env('apiUrl')}/businesses`, {
+                statusCode: 200,
+                body: businessCreatePostJson
+            })
+        })
+        cy.fixture('business-create-show').then((businessCreateShowJson) => {
+            cy.intercept(`${Cypress.env('apiUrl')}/businesses/4`, {
+                statusCode: 200,
+                body: businessCreateShowJson
+            })
+        })
+        cy.get('div.mt-5 > .has-background-primary-dark').click()
+
+        cy.get(':nth-child(3) > .message-header').should('be.visible').should('have.text', 'Business created successfully')
     })
 })
